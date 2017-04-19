a46e1f21b5a549e18733b4efe215fef43e27a250
ribeiromarcos <ultramarcos@gmail.com>
2017-04-19 16:12:55 -0300

    Add generator for ENDSEQ operator

---
+++ endseqgen.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python -u
+# -*- coding: utf-8 -*-
+
+'''
+Module for generation of environments for experiments over import data of
+soccer world cup of 2014 from http://data.huffingtonpost.com
+'''
+
+from tool.experiment import RAN, VAR, SLI, DEF, CQL_ALG, NAIVE_SUBSEQ_ALG, \
+    INC_SUBSEQ_ALG, PARAMETER, QUERY_LIST, Q_PLAY, DIRECTORY, \
+    ALGORITHM_LIST, Q_MOVE, gen_experiment_list
+from tool.io import ENDSEQ_MAIN_DIR, get_match_list, \
+    create_experiment_directories
+from tool.run import run_experiments, summarize_all, confidence_interval_all
+from tool.query.endseq import gen_all_queries, gen_all_env
+
+
+# =============================================================================
+# Experiment execution
+# =============================================================================
+# Number of experiment runs
+RUN_COUNT = 2
+# Match count
+MATCH_COUNT = 4
+
+# Parameters configuration
+ENDSEQ_PAR = {
+    # Range
+    RAN: {
+        VAR: [6, 12, 18, 24, 30],
+        DEF: 12
+        },
+    # Slide
+    SLI: {
+        VAR: [1, 3, 6, 9, 12],
+        DEF: 1
+        }
+    }
+
+ENDSEQ_CONF = {
+    # Algorithms
+    ALGORITHM_LIST: [CQL_ALG, INC_SUBSEQ_ALG, NAIVE_SUBSEQ_ALG],
+    # Query
+    QUERY_LIST: [Q_PLAY, Q_MOVE],
+    # Main directory
+    DIRECTORY: ENDSEQ_MAIN_DIR,
+    # Parameters
+    PARAMETER: ENDSEQ_PAR
+    }
+
+
+def get_arguments(print_help=False):
+    '''
+    Get arguments
+    '''
+    import argparse
+    parser = argparse.ArgumentParser('WC-ENDSEQ-Gen')
+    parser.add_argument('-g', '--gen', action="store_true",
+                        default=False,
+                        help='Generate files')
+    parser.add_argument('-o', '--output', action="store_true",
+                        default=False,
+                        help='Generate query output')
+    parser.add_argument('-r', '--run', action="store_true",
+                        default=False,
+                        help='Run experiments')
+    parser.add_argument('-s', '--summarize', action="store_true",
+                        default=False,
+                        help='Summarize results')
+    args = parser.parse_args()
+    if print_help:
+        parser.print_help()
+    return args
+
+
+def main():
+    '''
+    Main routine
+    '''
+    args = get_arguments()
+    print 'Getting list of matches'
+    match_list = get_match_list()[-MATCH_COUNT:]
+    exp_list = gen_experiment_list(ENDSEQ_CONF, match_list)
+    if args.gen:
+        create_experiment_directories(ENDSEQ_CONF, exp_list)
+        print 'Generating queries'
+        gen_all_queries(ENDSEQ_CONF, exp_list)
+        print 'Generating environments'
+        gen_all_env(ENDSEQ_CONF, exp_list, output=args.output)
+    elif args.run:
+        print 'Running experiments'
+        run_experiments(ENDSEQ_CONF, exp_list, RUN_COUNT)
+    elif args.summarize:
+        print 'Summarizing results'
+        summarize_all(ENDSEQ_CONF, match_list, RUN_COUNT)
+        print 'Calculating confidence intervals'
+        confidence_interval_all(ENDSEQ_CONF)
+    else:
+        get_arguments(True)
+
+
+if __name__ == '__main__':
+    main()
---
+++ tool/query/endseq.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+'''
+Queries for experiments with ENDSEQ operator
+'''
+
+import os
+
+from tool.attributes import get_play_attribute_list, get_move_attribute_list
+from tool.experiment import SLI, RAN, ALGORITHM, \
+    CQL_ALG, QUERY, Q_PLAY, Q_MOVE
+from tool.io import get_query_dir, write_to_txt, get_out_file, get_env_file
+from tool.query.stream import get_register_stream, REG_Q_OUTPUT_STR, REG_Q_STR
+
+
+# =============================================================================
+# Query using ENDSEQ operator
+# =============================================================================
+ENDSEQ_QUERY = '''
+SELECT SUBSEQUENCE END POSITION
+FROM SEQUENCE IDENTIFIED BY pid
+[RANGE {ran} SECOND, SLIDE {sli} SECOND] FROM s;
+'''
+
+# =============================================================================
+# CQL Queries
+# =============================================================================
+# Query to get sequence from stream
+CQL_Z = '''
+SELECT SEQUENCE IDENTIFIED BY pid [RANGE {ran} SECOND, SLIDE {sli} SECOND]
+FROM s;
+'''
+
+# Query equivalent to ENDSEQ operator
+CQL_EQUIV = '''
+SELECT _pos - {ran} + 1 AS _pos, {att} FROM z WHERE _pos >= {ran}
+'''
+
+
+def gen_endseq_query(configuration, experiment_conf):
+    '''
+    Generate queries with ENDSEQ operator
+    '''
+    query_dir = get_query_dir(configuration, experiment_conf)
+    filename = query_dir + os.sep + 'endseq.cql'
+    query = ENDSEQ_QUERY.format(ran=experiment_conf[RAN],
+                                sli=experiment_conf[SLI])
+    write_to_txt(filename, query)
+
+
+def gen_cql_z_query(query_dir, experiment_conf):
+    '''
+    Consider RANGE and SLIDE and generate Z relation
+    '''
+    query = CQL_Z.format(ran=experiment_conf[RAN],
+                         sli=experiment_conf[SLI])
+    filename = query_dir + os.sep + 'z.cql'
+    write_to_txt(filename, query)
+
+
+def gen_cql_final_query(query_dir, experiment_conf):
+    '''
+    Generate final query equivalent to ENDSEQ operator
+    '''
+    filename = query_dir + os.sep + 'equiv.cql'
+    if os.path.isfile(filename):
+        return
+    range_value = experiment_conf[RAN]
+    if experiment_conf[QUERY] == Q_PLAY:
+        att_list = get_play_attribute_list('z.')
+    elif experiment_conf[QUERY] == Q_MOVE:
+        att_list = get_move_attribute_list('z.')
+    att_str = ', '.join(att_list)
+    pos_query_list = []
+    for position in range(1, range_value + 1):
+        pos_query = CQL_EQUIV.format(att=att_str, ran=position)
+        pos_query_list.append(pos_query)
+    query = '\nUNION\n'.join(pos_query_list) + ';'
+    out_file = open(filename, 'w')
+    out_file.write(query)
+    out_file.close()
+
+
+def gen_cql_queries(configuration, experiment_conf):
+    '''
+    Generate all CQL queries equivalent to ENDSEQ operator
+    '''
+    query_dir = get_query_dir(configuration, experiment_conf)
+    gen_cql_z_query(query_dir, experiment_conf)
+    gen_cql_final_query(query_dir, experiment_conf)
+
+
+def gen_all_queries(configuration, experiment_list):
+    '''
+    Generate all queries
+    '''
+    for exp_conf in experiment_list:
+        if exp_conf[ALGORITHM] == CQL_ALG:
+            gen_cql_queries(configuration, exp_conf)
+        else:
+            gen_endseq_query(configuration, exp_conf)
+
+
+def gen_endseq_env(configuration, experiment_conf, output):
+    '''
+    Generate environment files for ENDSEQ operator
+    '''
+    text = get_register_stream(experiment_conf)
+    # Get query filename
+    query_dir = get_query_dir(configuration, experiment_conf)
+    filename = query_dir + os.sep + 'endseq.cql'
+    # Register query
+    if output:
+        # Get output filename
+        out_file = get_out_file(configuration, experiment_conf)
+        text += REG_Q_OUTPUT_STR.format(qname='endseq', qfile=filename,
+                                        ofile=out_file)
+    else:
+        text += REG_Q_STR.format(qname='endseq', qfile=filename)
+    # Get environment filename
+    filename = get_env_file(configuration, experiment_conf)
+    write_to_txt(filename, text)
+
+
+def gen_cql_env(configuration, experiment_conf, output):
+    '''
+    Generate environment files for StremPref
+    '''
+    text = get_register_stream(experiment_conf)
+    query_dir = get_query_dir(configuration, experiment_conf)
+    # Environment files for equivalent CQL queries
+    filename = query_dir + os.sep + 'z.cql'
+    text += REG_Q_STR.format(qname='z', qfile=filename)
+    # Final equivalent query
+    filename = query_dir + os.sep + 'equiv.cql'
+    if output:
+        # Get output filename
+        out_file = get_out_file(configuration, experiment_conf)
+        text += REG_Q_OUTPUT_STR.format(qname='equiv', qfile=filename,
+                                        ofile=out_file)
+    else:
+        text += REG_Q_STR.format(qname='equiv', qfile=filename)
+    filename = get_env_file(configuration, experiment_conf)
+    write_to_txt(filename, text)
+
+
+def gen_all_env(configuration, experiment_list, output=False):
+    '''
+    Generate all environment files
+    '''
+    for exp_conf in experiment_list:
+        if exp_conf[ALGORITHM] == CQL_ALG:
+            gen_cql_env(configuration, exp_conf, output)
+        else:
+            gen_endseq_env(configuration, exp_conf, output)
