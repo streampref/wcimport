b5a6b26805671eb61e8c7b9ef0d76914de69b3d6
ribeiromarcos <ultramarcos@gmail.com>
2017-04-22 11:38:45 -0300

    Add MAXSEQ generator

---
+++ maxseqgen.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python -u
+# -*- coding: utf-8 -*-
+
+'''
+Module for generation of environments for experiments over import data of
+soccer world cup of 2014 from http://data.huffingtonpost.com
+'''
+
+from tool.experiment import RAN, VAR, SLI, DEF, CQL_ALG, MAXSEQ_ALG,\
+    PARAMETER, QUERY_LIST, Q_PLAY, DIRECTORY, ALGORITHM_LIST, \
+    gen_experiment_list, Q_MOVE
+from tool.io import MAXSEQ_MAIN_DIR, get_match_list, \
+    create_experiment_directories
+from tool.query.seq import gen_all_queries, gen_all_env
+from tool.run import run_experiments, summarize_all, confidence_interval_all
+
+# =============================================================================
+# Experiment execution
+# =============================================================================
+# Number of experiment runs
+RUN_COUNT = 3
+# Match count
+MATCH_COUNT = 1
+
+# Parameters configuration
+MAXSEQ_PAR = {
+    # Range
+    RAN: {
+        VAR: [6, 12, 18, 24, 30],
+        DEF: 12
+        },
+    # Slide
+    SLI: {
+        VAR: [1, 3, 6, 9, 12],
+        DEF: 1
+        }
+    }
+
+MAXSEQ_CONF = {
+    # Algorithms
+    ALGORITHM_LIST: [CQL_ALG, MAXSEQ_ALG],
+    # Query
+    QUERY_LIST: [Q_PLAY, Q_MOVE],
+    # Main directory
+    DIRECTORY: MAXSEQ_MAIN_DIR,
+    # Parameters
+    PARAMETER: MAXSEQ_PAR
+    }
+
+
+def get_arguments(print_help=False):
+    '''
+    Get arguments
+    '''
+    import argparse
+    parser = argparse.ArgumentParser('MAXSEQGen')
+    parser.add_argument('-g', '--gen', action="store_true",
+                        default=False,
+                        help='Generate files')
+    parser.add_argument('-o', '--output', action="store_true",
+                        default=False,
+                        help='Generate query output')
+    parser.add_argument('-r', '--run', action="store_true",
+                        default=False,
+                        help='Run experiments')
+    parser.add_argument('-s', '--summarize', action="store_true",
+                        default=False,
+                        help='Summarize results')
+    args = parser.parse_args()
+    if print_help:
+        parser.print_help()
+    return args
+
+
+def main():
+    '''
+    Main routine
+    '''
+    args = get_arguments()
+    print 'Getting list of matches'
+    match_list = get_match_list()[-MATCH_COUNT:]
+    exp_list = gen_experiment_list(MAXSEQ_CONF, match_list)
+    if args.gen:
+        create_experiment_directories(MAXSEQ_CONF, exp_list)
+        print 'Generating queries'
+        gen_all_queries(MAXSEQ_CONF, exp_list)
+        print 'Generating environments'
+        gen_all_env(MAXSEQ_CONF, exp_list, output=args.output)
+    elif args.run:
+        print 'Running experiments'
+        run_experiments(MAXSEQ_CONF, exp_list, RUN_COUNT)
+    elif args.summarize:
+        print 'Summarizing results'
+        summarize_all(MAXSEQ_CONF, match_list, RUN_COUNT)
+        print 'Calculating confidence intervals'
+        confidence_interval_all(MAXSEQ_CONF)
+    else:
+        get_arguments(True)
+
+
+if __name__ == '__main__':
+    main()
---
+++ tool/query/maxseq.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+'''
+Queries for experiments with MAXSEQ operator
+'''
+
+import os
+
+from tool.io import write_to_txt, get_env_file, get_query_dir, \
+    get_out_file
+from tool.experiment import SLI, RAN, MAX, ALGORITHM, CQL_ALG
+from tool.query.stream import get_register_stream, REG_Q_OUTPUT_STR, REG_Q_STR
+
+
+# =============================================================================
+# Query using MAXSEQ operator
+# =============================================================================
+MAXSEQ_QUERY = '''
+SELECT SEQUENCE IDENTIFIED BY pid
+[RANGE {ran} SECOND, SLIDE {sli} SECOND] FROM s
+WHERE MAXIMUM SIZE IS {max}
+;
+'''
+
+# =============================================================================
+# CQL Queries
+# =============================================================================
+# Query to get sequence from stream
+CQL_Z = '''
+SELECT SEQUENCE IDENTIFIED BY pid [RANGE {ran} SECOND, SLIDE {sli} SECOND]
+FROM s;
+'''
+
+# Query to get sequence id having a maxium of positions
+CQL_ZMAX = '''
+SELECT DISTINCT pid FROM z
+EXCEPT
+SELECT pid FROM z WHERE _pos > {max};
+'''
+
+# Query equivalent to MAXSEQ operator
+CQL_EQUIV = '''
+SELECT z.* FROM z, zmax WHERE z.pid = zmax.pid;
+'''
+
+
+def gen_maxseq_query(configuration, experiment_conf):
+    '''
+    Generate queries with MAXSEQ operator
+    '''
+    query_dir = get_query_dir(configuration, experiment_conf)
+    filename = query_dir + os.sep + 'maxseq.cql'
+    query = MAXSEQ_QUERY.format(max=experiment_conf[MAX],
+                                ran=experiment_conf[RAN],
+                                sli=experiment_conf[SLI])
+    write_to_txt(filename, query)
+
+
+def gen_cql_queries(configuration, experiment_conf):
+    '''
+    Generate all CQL queries equivalent to MAXSEQ operator
+    '''
+    query_dir = get_query_dir(configuration, experiment_conf)
+    query = CQL_Z.format(ran=experiment_conf[RAN],
+                         sli=experiment_conf[SLI])
+    filename = query_dir + os.sep + 'z.cql'
+    write_to_txt(filename, query)
+    query = CQL_ZMAX.format(max=experiment_conf[MAX])
+    filename = query_dir + os.sep + 'zmax.cql'
+    write_to_txt(filename, query)
+    filename = query_dir + os.sep + 'equiv.cql'
+    write_to_txt(filename, CQL_EQUIV)
+
+
+def gen_all_queries(configuration, experiment_list):
+    '''
+    Generate all queries
+    '''
+    for exp_conf in experiment_list:
+        if exp_conf[ALGORITHM] == CQL_ALG:
+            gen_cql_queries(configuration, exp_conf)
+        else:
+            gen_maxseq_query(configuration, exp_conf)
+
+
+def gen_maxseq_env(configuration, experiment_conf, output):
+    '''
+    Generate environment files for MAXSEQ operator
+    '''
+    text = get_register_stream(experiment_conf)
+    # Get query filename
+    query_dir = get_query_dir(configuration, experiment_conf)
+    filename = query_dir + os.sep + 'maxseq.cql'
+    # Register query
+    if output:
+        # Get output filename
+        out_file = get_out_file(configuration, experiment_conf)
+        text += REG_Q_OUTPUT_STR.format(qname='maxseq', qfile=filename,
+                                        ofile=out_file)
+    else:
+        text += REG_Q_STR.format(qname='maxseq', qfile=filename)
+    # Get environment filename
+    filename = get_env_file(configuration, experiment_conf)
+    write_to_txt(filename, text)
+
+
+def gen_cql_env(configuration, experiment_conf, output):
+    '''
+    Generate environment files for StremPref
+    '''
+    text = get_register_stream(configuration, experiment_conf)
+    query_dir = get_query_dir(configuration, experiment_conf)
+    # Environment files for equivalent CQL queries
+    filename = query_dir + os.sep + 'z.cql'
+    text += REG_Q_STR.format(qname='z', qfile=filename)
+    filename = query_dir + os.sep + 'zmax.cql'
+    text += REG_Q_STR.format(qname='zmax', qfile=filename)
+    # Final equivalent query
+    filename = query_dir + os.sep + 'equiv.cql'
+    if output:
+        # Get output filename
+        out_file = get_out_file(configuration, experiment_conf)
+        text += REG_Q_OUTPUT_STR.format(qname='equiv', qfile=filename,
+                                        ofile=out_file)
+    else:
+        text += REG_Q_STR.format(qname='equiv', qfile=filename)
+    filename = get_env_file(configuration, experiment_conf)
+    write_to_txt(filename, text)
+
+
+def gen_all_env(configuration, experiment_list, output=False):
+    '''
+    Generate all environment files
+    '''
+    for exp_conf in experiment_list:
+        if exp_conf[ALGORITHM] == CQL_ALG:
+            gen_cql_env(configuration, exp_conf, output)
+        else:
+            gen_maxseq_env(configuration, exp_conf, output)
